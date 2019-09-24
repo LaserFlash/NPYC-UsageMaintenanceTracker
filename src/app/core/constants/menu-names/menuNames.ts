3b0e730b88e0cc291770df6160411c9cc0b58e7a
Bryn Bennett <bryn.t.bennett@gmail.com>
2019-09-24 18:39:35 +1200

    Remove 420 stuff that escaped TRIYA

--- src/app/core/constants/menu-names/menuNames.ts
+++ src/app/core/constants/menu-names/menuNames.ts
@@ -1,70 +1,62 @@
-
-export const Boats: number[] = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-];
+export const Boats: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export const UserFriendlyBoats: string[] = [
-  'PT1', 'PT2', 'PT3', 'PT4', 'PT5', 'PT6', 'PT7', 'Lois Dawn', 'Ngamotu'
+  "PT1",
+  "PT2",
+  "PT3",
+  "PT4",
+  "PT5",
+  "PT6",
+  "PT7",
+  "Lois Dawn",
+  "Ngamotu"
 ];
 
 export const Levels: string[] = [
-  'Urgent (out of action)',
-  'High (usable if needed)',
-  'Medium (boat usable)',
-  'Low (not affecting use)'
+  "Urgent (out of action)",
+  "High (usable if needed)",
+  "Medium (boat usable)",
+  "Low (not affecting use)"
 ];
 
 export const PartsRiB: string[] = [
-  'Hull',
-  'Pontoons',
-  'Engine',
-  'Steering',
-  'Safety Equipment',
-  'Prop',
-  'Radio',
-  'Safety Equipment',
-  'Other'
-];
-
-export const Parts420: string[] = [
-  'Hull',
-  'Rudder',
-  'Centre Plate',
-  'Control Lines',
-  'Spinnaker',
-  'Trapeze',
-  'Jib',
-  'Mainsail',
-  'Sheets',
-  'Other'
+  "Hull",
+  "Pontoons",
+  "Engine",
+  "Steering",
+  "Safety Equipment",
+  "Prop",
+  "Radio",
+  "Safety Equipment",
+  "Other"
 ];
 
 export const WindTypes: string[] = [
-  'None (0-3 knots)',
-  'Light (4-10 knots)',
-  'Moderate (11-27 knots)',
-  'Near Gale (28-33 knots)',
-  'Gale (34-39 knots)',
-  'Strong Gale (40+ knots)',
+  "None (0-3 knots)",
+  "Light (4-10 knots)",
+  "Moderate (11-27 knots)",
+  "Near Gale (28-33 knots)",
+  "Gale (34-39 knots)",
+  "Strong Gale (40+ knots)"
 ];
 
 export const WindDirection: string[] = [
-  'North',
-  'East',
-  'South',
-  'West',
-  'NE',
-  'NW',
-  'SE',
-  'SW'
+  "North",
+  "East",
+  "South",
+  "West",
+  "NE",
+  "NW",
+  "SE",
+  "SW"
 ];
 
 export const WaterState: string[] = [
-  'Calm (Glassy)',
-  'Calm (Rippled)',
-  'Smooth (0.25-0.5m)',
-  'Slight (0.5-1m)',
-  'Moderate (1-2m)',
-  'Rough (2-4m)',
-  'Very Rough (4-6m)'
+  "Calm (Glassy)",
+  "Calm (Rippled)",
+  "Smooth (0.25-0.5m)",
+  "Slight (0.5-1m)",
+  "Moderate (1-2m)",
+  "Rough (2-4m)",
+  "Very Rough (4-6m)"
 ];
